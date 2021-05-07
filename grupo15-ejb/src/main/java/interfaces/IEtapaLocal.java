3676566172c0ece8cc125b535630665c90e3bd3c
Valentin-VP <valevp2010@hotmail.com>
2021-05-07 11:40:06 -0300

    Interfaces de etapa

---
+++ grupo15-ejb/src/main/java/interfaces/IEtapaLocal.java
@@ -0,0 +1,18 @@
+package interfaces;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import datatypes.DtEtapa;
+import exceptions.EtapaInexistente;
+import exceptions.EtapaRepetida;
+import exceptions.PlanVacunacionInexistente;
+
+public interface IEtapaLocal {
+
+	public void agregarEtapa(int idEtapa, Date fIni, Date fFin, String cond, int idPlan) throws EtapaRepetida, PlanVacunacionInexistente;
+	
+	public ArrayList<DtEtapa> listarEtapas() throws EtapaInexistente;
+	
+	public DtEtapa obtenerEtapa(int id) throws EtapaInexistente;
+}
