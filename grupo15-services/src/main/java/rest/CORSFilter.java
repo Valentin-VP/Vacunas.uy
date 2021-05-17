9f023761d0fecfa14b4e4b3ccd5b822c463484c8
Rodrigo Castro <rcastro@netascom.com>
2021-05-16 22:41:11 -0300

    Print de CORS Filter

--- grupo15-services/src/main/java/rest/CORSFilter.java
+++ grupo15-services/src/main/java/rest/CORSFilter.java
@@ -1,6 +1,7 @@
 package rest;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
@@ -9,10 +10,11 @@ import javax.ws.rs.ext.Provider;
 
 @Provider
 public class CORSFilter implements ContainerResponseFilter {
-
+	private final Logger LOGGER = Logger.getLogger(getClass().getName());
    @Override
    public void filter(final ContainerRequestContext requestContext,
                       final ContainerResponseContext cres) throws IOException {
+	  LOGGER.info("Filtrando Response...");
       cres.getHeaders().add("Access-Control-Allow-Origin", "*");
       cres.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
       cres.getHeaders().add("Access-Control-Allow-Credentials", "true");
