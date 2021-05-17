7d3ab958d22f9950ebe943a1f5990899d2e34cb7
Rodrigo Castro Suarez <rodrigo.castro@fing.edu.uy>
2021-05-17 20:55:44 -0300

    Revert "Comunicacion con el Vacunatorio (soap), sin considerar nube"
    This reverts commit c09a6dfa25d01ae678c04d2a32635913367df220

--- grupo15-services/src/main/java/soap/AgendaSoap.java
+++ grupo15-services/src/main/java/soap/AgendaSoap.java
@@ -7,7 +7,7 @@ import javax.ejb.EJB;
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 
-import datatypes.DtReservaCompleto;
+import datatypes.DtReserva;
 import exceptions.AgendaInexistente;
 import interfaces.IAgendaDAOLocal;
 
@@ -18,7 +18,7 @@ public class AgendaSoap {
 	IAgendaDAOLocal ac;
 	
 	@WebMethod
-	public ArrayList<DtReservaCompleto> obtenerAgendaVacunatorio(String idVacunatorio) throws AgendaInexistente{
-		return ac.obtenerAgendaSoap(idVacunatorio, LocalDate.now());
+	public ArrayList<DtReserva> getAgendaVacunatorio(String idVacunatorio) throws AgendaInexistente{
+		return ac.obtenerAgenda(idVacunatorio, LocalDate.now());
 	}
 }
