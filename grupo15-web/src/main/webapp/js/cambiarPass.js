6d2f722355b67bede1112d9735c2570a40f01809
NOHELIA YANIBELLI <nyanibelli89@gmail.com>
2021-05-30 21:20:39 -0300

    Se sube la función para usuarios internos para modificar la
    password.

---
+++ grupo15-web/src/main/webapp/js/cambiarPass.js
@@ -0,0 +1,55 @@
+var pass = new Vue({
+    el: "#pass",
+    data: {
+      user: 'cookie',
+      pass: '',
+      codigo200:''
+    },
+   
+  
+  
+    methods: {
+      setDatos() {
+  
+        
+        console.log("user:" + this.user);
+        pass = this.pass;
+        console.log("pass:" + this.pass);
+        this.setPass();
+  
+      },
+     
+      setPass() {
+        tok = this.pass;
+        hash = Base64.encode(tok);
+        console.log("Tok:" + this.tok);
+        console.log("Hash:" + this.hash);
+       
+      
+       axios.post('http://localhost:8080/grupo15-services/rest/internalauth/login', { 
+        pass : this.hash.toString(),
+       
+      })
+      
+        .then(response =>{
+          if (response.data.status=== 200){
+            console.log("Respuesta: " + response.data)
+            this.codigo200= response.data;
+          }
+          
+        }
+        )
+      //  .catch(error => {
+      //    if (error.response.status === 401) {
+   		//    console.log("Acceso no permitido. Verifique credenciales");
+ 		 // }
+       //   else{
+         // 	console.log(error)
+         // }
+       // })
+   }
+  
+    }
+  
+  })
+  
