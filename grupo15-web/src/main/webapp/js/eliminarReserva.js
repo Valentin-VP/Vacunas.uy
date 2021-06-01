b3174706fcb79449b6f40f73f3e1a8b1779317c5
Nicolas Mendez Mañana <nicolas.mendez.manana@fing.edu.uy>
2021-06-01 19:22:43 -0300

    eliminarReserva.html: @change
    eliminarReserva.js:
    updateIndex(event, selectedIndex): get index seleccionado de select
    getReservas(): test catch

--- grupo15-web/src/main/webapp/js/eliminarReserva.js
+++ grupo15-web/src/main/webapp/js/eliminarReserva.js
@@ -1,64 +1,70 @@
 var res = new Vue({
-  el: "#res",
-  data: {
-   
-    listaReservas: [],
-    reserva:''
-  
-  },
-  mounted() {
-    this.getReservas();
-  },
+	el: "#res",
+	data: {
+
+		listaReservas: [],
+		reserva: '',
+		selectedIndex: ''
+	},
+	mounted() {
+		this.getReservas();
+	},
 
 
 
 
-    methods: 
-    {
-      aEliminar() {
-        
-        reserva=this.reserva;
-        console.log('lista Eliminar: ', this.reserva);
-        this.eliminarDatos();
-        
-      },
-      
-    //  eliminarDatos () {
+	methods:
+	{
+		aEliminar() {
 
-    
-      
-    // axios.delete("/grupo15-services/rest/reservas/eliminar?p=idPlan&e=idEtapa&date=yyyy-MM-dd", {
+			reserva = this.reserva;
+			console.log('lista Eliminar: ', this.reserva);
+			this.eliminarDatos();
 
-    //  reserva : this.reserva.toString(),
-    
-     
-   //});
-  
-getReservas () {
-        
-         
-  axios.get("http://localhost:8080/grupo15-services/rest/reservas/listar")
-  .then((response => {
-
-    console.log('lista Reservas: ', response.data)
-    this.listaReservas = response.data;
+		},
+		updateIndex(event, selectedIndex) {
+			console.log(event, selectedIndex);
+			console.log("/grupo15-services/rest/reservas/eliminar?e=" + this.listaReservas[selectedIndex - 1].enfermedad.toString() + "&date=" + this.listaReservas[selectedIndex - 1].fecha.toString());
+			this.selectedIndex = selectedIndex;
+		},
+		eliminarDatos() {
 
 
-  }))
+			if (this.listaReservas[this.selectedIndex - 1]){
+				axios.delete("/grupo15-services/rest/reservas/eliminar?e=" + this.listaReservas[this.selectedIndex - 1].enfermedad.toString() + "&date=" + this.listaReservas[this.selectedIndex - 1].fecha.toString()).then(response => {
+				
+					this.getReservas();
+				})
+			}
+			
+		},
+
+		getReservas() {
 
 
-      },
+			axios.get("http://localhost:8080/grupo15-services/rest/reservas/listar")
+				.then(response => {
 
-      
+					console.log('lista Reservas: ', response.data)
+					this.listaReservas = response.data;
 
-            },
 
-        
+				}).catch(error=>{
+					console.log(error.response)})
+					this.listaReservas = []
 
-            
+		},
 
-              
-  
-     
-  
-   })
+
+
+	},
+
+
+
+
+
+
+
+
+
+})
