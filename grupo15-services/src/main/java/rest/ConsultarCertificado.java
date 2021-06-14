adb525906ded9a7dc29f6453a0fc373f6b19850b
Valentin-VP <valevp2010@hotmail.com>
2021-06-14 18:41:35 -0300

    se agrega certificados especial para vue

--- grupo15-services/src/main/java/rest/ConsultarCertificado.java
+++ grupo15-services/src/main/java/rest/ConsultarCertificado.java
@@ -14,9 +14,13 @@ import javax.ws.rs.core.Cookie;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.jose4j.jwt.consumer.InvalidJwtException;
 
 import datatypes.DtCertificadoVac;
+import datatypes.DtConstancia;
 import exceptions.CertificadoInexistente;
 import exceptions.UsuarioExistente;
 import interfaces.ICertificadoVacunacionDAOLocal;
@@ -59,6 +63,35 @@ public class ConsultarCertificado {
 			return Response.ok(dtCV).build();
 		}
 		
+		@GET
+		@Path("/certificadovue")
+		@RolesAllowed("ciudadano")
+		public Response getCertificadovue(@CookieParam("x-access-token") Cookie cookie) {
+			String token = cookie.getValue();
+			String ci = null;
+			try {
+				ci = TokenSecurity.getIdClaim(TokenSecurity.validateJwtToken(token));
+			} catch (InvalidJwtException e) {
+				e.printStackTrace();
+			}
+			DtCertificadoVac dtCV;
+			try {
+				dtCV = ICertVac.obtenerCertificadoVacunacion(Integer.parseInt(ci));
+				JSONArray certificado = new JSONArray();
+				for(DtConstancia c: dtCV.getConstancias()) {
+					JSONObject datos = new JSONObject();
+					datos.put("ci", ci);
+					datos.put("vacuna", c.getVacuna());
+					datos.put("enfermedad", c.getEnfermedad());
+					datos.put("pInmunidad", c.getPeriodoInmunidad());
+					datos.put("totalDosis", c.getDosisRecibidas());
+					datos.put("ultimaDosis", c.getFechaUltimaDosis().toString());
+				}
+				return Response.ok(certificado).build();
+			} catch (NumberFormatException | CertificadoInexistente | UsuarioExistente | JSONException error) {
+				return ResponseBuilder.createResponse(Response.Status.BAD_REQUEST, error.getMessage());
+			}
+		}
 		
 
 }
