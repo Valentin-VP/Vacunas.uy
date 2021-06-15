f24ae991f9db1bdb7d9bb1aea813e0b150eac3a3
NOHELIA YANIBELLI <nyanibelli89@gmail.com>
2021-06-14 21:02:01 -0300

    certificado vacunacion terminado

--- grupo15-web/src/main/webapp/js/certificadoVacunacion.js
+++ grupo15-web/src/main/webapp/js/certificadoVacunacion.js
@@ -2,6 +2,7 @@
 var vac = new Vue({
   el: "#vac",
   data: {
+    idEnf:'',
     enfermedad: '',
     id: '',
     inmunidad: '',
@@ -9,6 +10,7 @@ var vac = new Vue({
     ultimaDosis: '',
     vacuna: '',
     listConstancias: [],
+    listEnf: [],
   },
   mounted() {
     this.traerDatos();
@@ -18,32 +20,58 @@ var vac = new Vue({
   methods: {
     setDatos() {
 
-      enf = this.enfermedad;
+      enfermedad = this.enfermedad;
       console.log("enfermedad seleccionada:" + this.enfermedad);
-
+      this.obtenerCertificado();
 
     },
     traerDatos() {
-      axios.get("http://localhost:8080/grupo15-services/rest/vacunaciones/certificado")
+      axios.get("http://localhost:8080/grupo15-services/rest/vacunaciones/certificadovue")
         .then((response => {
 
           console.log(`lista constancias: `, response.data)
           this.listConstancias = response.data
 
           console.log(`lista constancias: `, this.listConstancias)
+          this.cargarEnfermedad();
         }))
     },
     
 
 
-    cargarDatos() {
+    cargarEnfermedad() {
 
 
       for (i in this.listConstancias) {
 
-        this.etiquetas.push(this.listConstancias[i].idEnfermedad)
+        this.listEnf.push(this.listConstancias[i].enfermedad)
       };
+      console.log(`lista Enfermedades: `, this.listEnf)
+    
 
+    },
+
+    obtenerCertificado() {
+
+
+      for (i in this.listConstancias) {
+        if (this.listConstancias[i].enfermedad = enfermedad){
+          this.idEnf = this.enfermedad;
+          this.id= this.listConstancias[i].ci;
+          this.inmunidad= this.listConstancias[i].pInmunidad;
+          this.totalDosis=this.listConstancias[i].totalDosis;
+          this.ultimaDosis=this.listConstancias[i].ultimaDosis;
+          this.vacuna=this.listConstancias[i].vacuna;
+          console.log(`lista listEnf: `, this.idEnf);
+          console.log(`lista id: `, this.id);
+          console.log(`lista inmunidad: `, this.inmunidad);
+          console.log(`lista totalDosis: `, this.totalDosis);
+          console.log(`lista UltimaDosis: `, this.ultimaDosis);
+          console.log(`lista Vacuna: `, this.vacuna);
+        }
+        
+      };
+  
     
 
     }
