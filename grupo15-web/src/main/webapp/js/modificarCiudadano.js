eec74b892dccce8b01518e15559492a29a602d75
NOHELIA YANIBELLI <nyanibelli89@gmail.com>
2021-06-16 16:27:32 -0300

    modificar ciudadano con datos cargados en el input

--- grupo15-web/src/main/webapp/js/modificarCiudadano.js
+++ grupo15-web/src/main/webapp/js/modificarCiudadano.js
@@ -10,36 +10,15 @@ var mod = new Vue({
     email: '',
     departamento: '',
     barrio: '',
-    iniDireccion: '',
-    iniBarrio: '',
-    iniEmail: '',
-    iniDepartamento: '',
   }),
   mounted() {
     this.cargarDatosIniciales();
-    iniDireccion = this.iniDireccion;
-    iniBarrio = this.iniBarrio;
-    iniEmail = this.iniEmail;
-    iniDepartamento = this.iniDepartamento;
   },
 
   methods:
   {
 
     SetDatos() {
-
-
-      email = this.email;
-      console.log("Email:" + this.email);
-      direccion = this.direccion;
-      console.log("Direccion: " + this.direccion);
-      departamento = this.departamento;
-      console.log("Departamento: " + this.departamento);
-      barrio = this.barrio;
-      console.log("Barrio: " + this.barrio);
-
-
-
       axios.post("http://localhost:8080/grupo15-services/rest/usuario/ciudadano/modificar", {
 
         email: this.email.toString(),
@@ -48,18 +27,18 @@ var mod = new Vue({
         barrio: this.barrio.toString()
       });
     },
-    cargarDatosIniciales() {
 
+    cargarDatosIniciales() {
+      let _this = this
       axios.get("http://localhost:8080/grupo15-services/rest/usuario/ciudadano/datosModificar")
         .then((response => {
-          console.log("GET rest: ", response.data.barrio)
-          this.iniBarrio= response.data.barrio;
-          this.iniDepartamento= response.data.departamento;
-          this.iniDireccion= response.data.direccion;
-          this.iniEmail= response.data.mail;
+          console.log("GET rest: ", response.data)
+          _this.barrio= response.data.barrio;
+          _this.departamento= response.data.departamento;
+          _this.direccion= response.data.direccion;
+          _this.email= response.data.mail;
 
         }))
-
     }
   }
 })
