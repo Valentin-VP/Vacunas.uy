1f858c8fb137ac09a30b04280587d55a64746d7f
NOHELIA YANIBELLI <nyanibelli89@gmail.com>
2021-06-15 21:37:00 -0300

    Registro ciudadano con redireccion mas menu ciudadano con modificar.

--- grupo15-web/src/main/webapp/js/RegistroCiudadano.js
+++ grupo15-web/src/main/webapp/js/RegistroCiudadano.js
@@ -1,33 +1,33 @@
 var res = new Vue({
-    el: "#res",
-    data: () => ({
-      id:'',
-      nombre:'',
-      apellido:'',
-      direccion:'',
-      fecha:'',
-      sexo:'',
-      email:'',
-      departamento: '',
-      barrio:''
+  el: "#res",
+  data: () => ({
+    id: '',
+    nombre: '',
+    apellido: '',
+    direccion: '',
+    fecha: '',
+    sexo: '',
+    email: '',
+    departamento: '',
+    barrio: ''
 
-    }),
+  }),
 
-    methods: 
-    {
-      
-      SetDatos () {
-      id=this.id;
+  methods:
+  {
+
+    SetDatos() {
+      id = this.id;
       console.log("IdEnf:" + this.id);
-      nombre=this.nombre;
+      nombre = this.nombre;
       console.log("IdPlan:" + this.nombre);
-      apellido= this.apellido;
+      apellido = this.apellido;
       console.log("IdVac:" + this.apellido);
-      fecha=this.fecha;
+      fecha = this.fecha;
       console.log("Fecha:" + this.fecha);
-      sexo=this.sexo;
+      sexo = this.sexo;
       console.log("Sexo:" + this.sexo);
-      email=this.email;
+      email = this.email;
       console.log("Email:" + this.email);
       direccion = this.direccion;
       console.log("Direccion: " + this.direccion);
@@ -35,22 +35,29 @@ var res = new Vue({
       console.log("Departamento: " + this.departamento);
       barrio = this.barrio;
       console.log("Barrio: " + this.barrio);
-      
+
       axios.post("http://localhost:8080/grupo15-services/rest/registro/ciudadano", {
-        id : this.id.toString(),
-        nombre : this.nombre.toString(),
-        apellido : this.apellido.toString(),
-        fechaNac : this.fecha.toString(),
-        sexo : this.sexo.toString(),
-         email : this.email.toString(),
+        id: this.id.toString(),
+        nombre: this.nombre.toString(),
+        apellido: this.apellido.toString(),
+        fechaNac: this.fecha.toString(),
+        sexo: this.sexo.toString(),
+        email: this.email.toString(),
         direccion: this.direccion.toString(),
         departamento: this.departamento.toString(),
         barrio: this.barrio.toString()
-      });
-   
-  
-        }
+      })
+        .then(response => {
+          if (response.status === 201) {
+            console.log("Respuesta: " + response.status)
+            window.location.href = "http://localhost:8080/grupo15-web/html/MenuCiudadano.html";
+          }
+
+
+        });
+
     }
-     
-  
+
+
+  }
 })
