af72c9b1a2e4a9d6f424ef2462f249868d27fe07
NOHELIA YANIBELLI <nyanibelli89@gmail.com>
2021-06-15 21:45:27 -0300

    Modificar ciudadano terminado

--- grupo15-web/src/main/webapp/js/modificarCiudadano.js
+++ grupo15-web/src/main/webapp/js/modificarCiudadano.js
@@ -1,44 +1,68 @@
 var mod = new Vue({
   el: "#mod",
   data: () => ({
-    id:'',
-    nombre:'',
-    apellido:'',
-    direccion:'',
-    fecha:'',
-    sexo:'',
-    email:'',
+    id: '',
+    nombre: '',
+    apellido: '',
+    direccion: '',
+    fecha: '',
+    sexo: '',
+    email: '',
     departamento: '',
-    barrio:''
-
+    barrio: '',
+    iniDireccion: '',
+    iniBarrio: '',
+    iniEmail: '',
+    iniDepartamento: '',
   }),
+  mounted() {
+    this.cargarDatosIniciales();
+    iniDireccion = this.iniDireccion;
+    iniBarrio = this.iniBarrio;
+    iniEmail = this.iniEmail;
+    iniDepartamento = this.iniDepartamento;
+  },
 
-  methods: 
+  methods:
   {
-    
-    SetDatos () {
-    
 
-    email=this.email;
-    console.log("Email:" + this.email);
-    direccion = this.direccion;
-    console.log("Direccion: " + this.direccion);
-    departamento = this.departamento;
-    console.log("Departamento: " + this.departamento);
-    barrio = this.barrio;
-    console.log("Barrio: " + this.barrio);
-    
-    axios.post("http://localhost:8080/grupo15-services/rest/registro/ciudadano", {
-      
-       email : this.email.toString(),
-      direccion: this.direccion.toString(),
-      departamento: this.departamento.toString(),
-      barrio: this.barrio.toString()
-    });
- 
+    SetDatos() {
 
-      }
+
+      email = this.email;
+      console.log("Email:" + this.email);
+      direccion = this.direccion;
+      console.log("Direccion: " + this.direccion);
+      departamento = this.departamento;
+      console.log("Departamento: " + this.departamento);
+      barrio = this.barrio;
+      console.log("Barrio: " + this.barrio);
+
+
+
+      axios.post("http://localhost:8080/grupo15-services/rest/usuario/ciudadano/modificar", {
+
+        email: this.email.toString(),
+        direccion: this.direccion.toString(),
+        departamento: this.departamento.toString(),
+        barrio: this.barrio.toString()
+      });
+    },
+    cargarDatosIniciales() {
+
+      axios.get("http://localhost:8080/grupo15-services/rest/usuario/ciudadano/datosModificar")
+        .then((response => {
+          console.log("GET rest: ", response.data.barrio)
+          this.iniBarrio= response.data.barrio;
+          this.iniDepartamento= response.data.departamento;
+          this.iniDireccion= response.data.direccion;
+          this.iniEmail= response.data.mail;
+
+        }))
+
+    }
   }
-   
-
 })
+
+
+
