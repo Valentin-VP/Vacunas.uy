366f5a3f84cb8aec6bef8062d3cd9c65bd086eb4
Rodrigo <rodrigo.castro@fing.edu.uy>
2021-06-21 22:35:36 -0300

    Login Falso con tipo Usuario

--- grupo15-services/src/main/java/rest/LoginFalso.java
+++ grupo15-services/src/main/java/rest/LoginFalso.java
@@ -18,6 +18,9 @@ import javax.ws.rs.core.Response;
 import org.jose4j.lang.JoseException;
 
 import datatypes.DtCiudadano;
+import datatypes.DtUsuarioInterno;
+import datatypes.DtVacunador;
+import datatypes.Rol;
 import exceptions.UsuarioExistente;
 import exceptions.UsuarioInexistente;
 import interfaces.IUsuarioLocal;
@@ -50,6 +53,49 @@ public class LoginFalso {
 			dt = IUsuarioLocal.buscarCiudadano(Integer.parseInt(ci));
 			dt.setToken(token);
 			IUsuarioLocal.ModificarCiudadano(dt);
+			NewCookie cookie = new NewCookie("x-access-token", token);
+			return Response.ok().cookie(cookie).build();
+		} catch (NumberFormatException | UsuarioExistente | JoseException | UsuarioInexistente e) {
+			return ResponseBuilder.createResponse(Response.Status.BAD_REQUEST, "");
+		}
+	}
+	
+	@GET
+	@Path("/fakeusuario")
+	@PermitAll
+	public Response getTokenUsuario(@QueryParam("ci") String ci, @QueryParam("tipo") String tipo) {
+		try {
+			String token = TokenSecurity.generateJwtToken(ci, tipo);
+			switch(tipo){
+				case "ciudadano":
+					IUsuarioLocal.agregarUsuarioCiudadano(Integer.parseInt(ci), null, null, null, null, null, null, null, false);
+					DtCiudadano ciu;
+					ciu = IUsuarioLocal.buscarCiudadano(Integer.parseInt(ci));
+					ciu.setToken(token);
+					IUsuarioLocal.ModificarCiudadano(ciu);
+					break;
+				case "autoridad":
+					IUsuarioLocal.agregarUsuarioInterno(Integer.parseInt(ci), ci, tipo, null, null, null, null, Rol.Autoridad);
+					DtUsuarioInterno aut;
+					aut = IUsuarioLocal.buscarUsuarioInterno(Integer.parseInt(ci));
+					aut.setToken(token);
+					IUsuarioLocal.ModificarUsuarioInterno(aut);
+					break;
+				case "administrador":
+					IUsuarioLocal.agregarUsuarioInterno(Integer.parseInt(ci), ci, tipo, null, null, null, null, Rol.Administrador);
+					DtUsuarioInterno adm;
+					adm = IUsuarioLocal.buscarUsuarioInterno(Integer.parseInt(ci));
+					adm.setToken(token);
+					IUsuarioLocal.ModificarUsuarioInterno(adm);
+					break;
+				case "vacunador":
+					IUsuarioLocal.agregarUsuarioVacunador(Integer.parseInt(ci), ci, tipo, null, token, null, null);
+					DtVacunador vac;
+					vac = IUsuarioLocal.buscarVacunador(Integer.parseInt(ci));
+					vac.setToken(token);
+					IUsuarioLocal.ModificarVacunador(vac);
+					break;
+			}
 			NewCookie cookie = new NewCookie("x-access-token", token);
 			return Response.ok().cookie(cookie).build();
 		} catch (NumberFormatException | UsuarioExistente | JoseException | UsuarioInexistente e) {
