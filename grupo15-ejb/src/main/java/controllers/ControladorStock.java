51347db60375982300d9c790c9ccee5592ed636b
Nicolas Mendez Mañana <nicolas.mendez.manana@fing.edu.uy>
2021-06-24 00:45:34 -0300

    Contacionalismo de historico prototipico

--- grupo15-ejb/src/main/java/controllers/ControladorStock.java
+++ grupo15-ejb/src/main/java/controllers/ControladorStock.java
@@ -2,13 +2,18 @@ package controllers;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import datatypes.DtHistoricoStock;
 import datatypes.DtStock;
@@ -162,4 +167,103 @@ public class ControladorStock implements IStockDaoLocal, IStockDaoRemote {
 		return stockVacunatorio;
 	}
 
+	private String prepararQueryStockGlobalActual(String enfermedad, String vacuna){
+		String base = "SELECT s FROM STOCK s";
+		if (vacuna!=null && !vacuna.equals("")) {
+			base.concat(" WHERE vacuna = '" + vacuna + "'");
+		}else {
+			if (enfermedad!=null && !enfermedad.equals("")) {
+				Query query = em.createQuery("SELECT v FROM Vacuna v WHERE enfermedad_nombre = '"+vacuna + "'");
+				@SuppressWarnings("unchecked")
+				List<Vacuna> vacunas = (List<Vacuna>) query.getResultList();
+				int size = 0;
+				if (!vacunas.isEmpty()) {
+					base.concat(" WHERE ");
+					size = vacunas.size();
+				}
+				int i = 0;
+				for (Vacuna v: vacunas) {
+					i++;
+					base.concat("vacuna = '" + v.getNombre() + "'");
+					if (i<=size) {
+						base.concat(" OR ");
+					}
+				}
+			}//else{}
+		}
+		return base;
+	}
+	
+	public Map<String, String> getStockGlobal(String enfermedad, String vacuna, String vacunatorio) {
+		Map<String, String> retorno = new HashMap<String, String>();
+		Query query;
+		if (vacunatorio!=null && !vacunatorio.equals("")) {
+			query = em.createQuery(prepararQueryStockGlobalActual(enfermedad, vacuna).concat(" AND vacunatorio = '" + vacunatorio + "'"));
+		}else {
+			query = em.createQuery(prepararQueryStockGlobalActual(enfermedad, vacuna));
+		}
+		@SuppressWarnings("unchecked")
+		List<Stock> stocks = query.getResultList();
+		if (!stocks.isEmpty()) {
+			int cantTotal = 0;
+			int cantAdmin = 0;
+			int cantDesc = 0;
+			int cantDisp = 0;
+			for (Stock s: stocks) {
+				cantTotal += s.getCantidad();
+				cantAdmin += s.getAdministradas();
+				cantDesc += s.getDescartadas();
+				cantDisp += s.getDisponibles();
+			}
+			retorno.put("cantTotal", String.valueOf(cantTotal));
+			retorno.put("cantAdmin", String.valueOf(cantAdmin));
+			retorno.put("cantDesc", String.valueOf(cantDesc));
+			retorno.put("cantDisp", String.valueOf(cantDisp));
+		}else {
+			retorno.put("cantTotal", String.valueOf(0));
+			retorno.put("cantAdmin", String.valueOf(0));
+			retorno.put("cantDesc", String.valueOf(0));
+			retorno.put("cantDisp", String.valueOf(0));
+		}
+		return retorno;
+	}
+	
+	public Map<String, Map<String, String>> getHistoricoStock(String enfermedad, String vacuna, String vacunatorio, LocalDate fechaInicio, LocalDate fechaFin){
+		Map<String, Map<String, String>> retorno = new HashMap<String, Map<String, String>>();
+		Map<String, String> valores;
+		Query query;
+		if (vacunatorio!=null && !vacunatorio.equals("")) {
+			query = em.createQuery(prepararQueryStockGlobalActual(enfermedad, vacuna).concat(" AND vacunatorio = '" + vacunatorio + "'"));
+		}else {
+			query = em.createQuery(prepararQueryStockGlobalActual(enfermedad, vacuna));
+		}
+		@SuppressWarnings("unchecked")
+		List<Stock> stocks = query.getResultList();
+		if (!stocks.isEmpty()) {
+			int barra = 0;
+			for (Stock s: stocks) {
+				List <Historico> historicos = s.getHistoricos();
+		        for (Historico h: historicos){
+		        	if (!h.getFecha().isBefore(fechaInicio) && !h.getFecha().isAfter(fechaFin)) {
+		        		valores = new HashMap<String, String>();
+		        		barra = h.getFecha().getMonthValue();
+						valores.put("cantTotal", String.valueOf(Integer.parseInt(retorno.get(String.valueOf(barra)).get("cantTotal")) + h.getCantidad()));
+						valores.put("cantAdmin", String.valueOf(Integer.parseInt(retorno.get(String.valueOf(barra)).get("cantAdmin")) + h.getAdministradas()));
+						valores.put("cantDesc", String.valueOf(Integer.parseInt(retorno.get(String.valueOf(barra)).get("cantDesc")) + h.getDescartadas()));
+						valores.put("cantDisp", String.valueOf(Integer.parseInt(retorno.get(String.valueOf(barra)).get("cantDisp")) +  h.getDisponibles()));
+						retorno.put(String.valueOf(barra), valores);
+		        	}
+		        }
+			}
+		}else {
+			valores = new HashMap<String, String>();
+			valores.put("cantTotal", String.valueOf(0));
+			valores.put("cantAdmin", String.valueOf(0));
+			valores.put("cantDesc", String.valueOf(0));
+			valores.put("cantDisp", String.valueOf(0));
+			retorno.put("0", valores);
+		}
+		return retorno;
+		
+	}
 }
